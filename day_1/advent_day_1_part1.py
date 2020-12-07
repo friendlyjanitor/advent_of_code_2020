9d6557ff7a1f5e8997f47a6a84b8f23599534521
Ryan Cross <rcross@kyruus.com>
2020-12-06 21:01:14 -0500

    adding advent day 1

---
+++ day_1/advent_day_1_part1.py
@@ -0,0 +1,19 @@
+import itertools
+import numpy
+import fileinput
+
+def generate_list():
+    expense_report = []
+    for line in fileinput.input():
+        expense_report.append(int(line.strip('\n')))
+    return expense_report
+
+def sum_2020(expense_report, r):
+    combinations = itertools.combinations(expense_report, r)
+    for comb in combinations:
+        if sum(comb) == 2020:
+            print numpy.prod(comb)
+
+
+expense_report = generate_list()
+sum_2020(expense_report, 2)
